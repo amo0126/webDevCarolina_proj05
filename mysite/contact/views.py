c61796358bc91fab46db25968d769c42e402515f
Raghuvara Padma <raghuvarapadma@gmail.com>
2020-10-15 19:41:40 -0400

    forms done

---
+++ mysite/contact/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from .forms import ResponseModelForm
+from django.core.mail import send_mail
+import django.core.mail as h
+
+
+# Create your views here.
+
+def contact_us(request):
+    form = ResponseModelForm(request.POST)
+    if request.method == "POST":
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            connect = h.get_connection()
+            connect.send_mail('Hello', 'Hello', email, ['raghuvarapadma@gmail.com'])
+            connect.close()
+            form.save()
+    return render(request, 'contact/contact_us.html', {'form': form})
